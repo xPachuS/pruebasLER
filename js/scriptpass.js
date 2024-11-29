1445bb7fe48f64d33069c1823411a9aa32f0544d
xPachuS <albertouge@gmail.com>
2024-11-29 11:51:33 +0100

    Add files via upload

---
+++ js/scriptpass.js
@@ -0,0 +1,19 @@
+const correctHash = "0f010a850a18b0c222a742be9399177e40b026f1a21b7e79fcfb46bac8a6fc71"; // Hash de "Madrid2024"
+
+async function checkPassword() {
+    const enteredPassword = document.getElementById('password').value;
+
+    // Generar el hash SHA-256 de la contraseña ingresada
+    const encoder = new TextEncoder();
+    const data = encoder.encode(enteredPassword);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const enteredHash = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+
+    // Comparar los hashes
+    if (enteredHash === correctHash) {
+        window.location.href = "pruebatres.html"; // Cambia esto por la página protegida
+    } else {
+        document.getElementById('error').textContent = 'Contraseña incorrecta, por favor intente de nuevo.';
+    }
+}
