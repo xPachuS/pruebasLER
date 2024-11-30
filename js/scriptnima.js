2bd8072a28c44bfcb004c451ad030ea15c136d9b
xPachuS <albertouge@gmail.com>
2024-11-30 22:28:02 +0100

    Add files via upload

---
+++ js/scriptnima.js
@@ -0,0 +1,69 @@
+// Data for cards
+const cardsData = [
+    { title: "Andalucía", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://www.juntadeandalucia.es/medioambiente/sira-buscador-publico/' target='_blank'>aquí</a>.", modalId: "modal1", iframeSrc: "https://www.juntadeandalucia.es/medioambiente/sira-buscador-publico/", backgroundImage: "andalucia.png" },
+    { title: "Aragón", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://aplicaciones2.aragon.es/pdr/pdr_pub/residuos/informacionAmbiental/busquedaNimas' target='_blank'>aquí</a>.", modalId: "modal2", iframeSrc: "https://aplicaciones2.aragon.es/pdr/pdr_pub/residuos/informacionAmbiental/busquedaNimas", backgroundImage: "aragon.png" },
+    { title: "Asturias", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://www.medioambiente.asturias.org/IASERVICIORESIDUOS/Buscar/BuscarInstalacion' target='_blank'>aquí</a>.", modalId: "modal3", iframeSrc: "https://www.medioambiente.asturias.org/IASERVICIORESIDUOS/Buscar/BuscarInstalacion", backgroundImage: "asturias.png"  },
+    { title: "Cantabria", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://siacan.cantabria.es/siacan/publico/PrepareBuscadorEmpresasGestoresView.do' target='_blank'>aquí</a>.", modalId: "modal4", iframeSrc: "https://siacan.cantabria.es/siacan/publico/PrepareBuscadorEmpresasGestoresView.do", backgroundImage: "cantabria.png"  },
+    { title: "Castilla y León", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://servicios.jcyl.es/gaser/verFrmBuscadorGestores.action' target='_blank'>aquí</a>.", modalId: "modal5", iframeSrc: "https://servicios.jcyl.es/gaser/verFrmBuscadorGestores.action", backgroundImage: "cyl.png"  },
+    { title: "Castilla la Mancha", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://ireno.castillalamancha.es/forms/geref000.htm' target='_blank'>aquí</a>.", modalId: "modal6", iframeSrc: "https://ireno.castillalamancha.es/forms/geref000.htm", backgroundImage: "clm.png"  },
+    { title: "Cataluña", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://sdr.arc.cat/modemp/ListGestors.do' target='_blank'>aquí</a>.", modalId: "modal7", iframeSrc: "https://sdr.arc.cat/modemp/ListGestors.do", backgroundImage: "catalunya.png"  },
+    { title: "Com. de Madrid", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://gestiona.comunidad.madrid/pcea_nima_web/html/web/InicioAccion.icm' target='_blank'>aquí</a>.", modalId: "modal8", iframeSrc: "https://gestiona.comunidad.madrid/pcea_nima_web/html/web/InicioAccion.icm", backgroundImage: "madrid.png"  },
+    { title: "Com. Valenciana", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://residuos.gva.es/res_buscaweb/buscador_residuos_avanzado.aspx' target='_blank'>aquí</a>.", modalId: "modal9", iframeSrc: "https://residuos.gva.es/res_buscaweb/buscador_residuos_avanzado.aspx", backgroundImage: "valenciana.png"  },
+    { title: "Extremadura", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='http://extremambiente.juntaex.es/index.php?option=com_content&id=2563' target='_blank'>aquí</a>.", modalId: "modal10", iframeSrc: "http://extremambiente.juntaex.es/index.php?option=com_content&id=2563", backgroundImage: "extremadura.png"  },
+    { title: "Galicia", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://sirga.xunta.gal/nima' target='_blank'>aquí</a>.", modalId: "modal11", iframeSrc: "https://sirga.xunta.gal/nima", backgroundImage: "galicia.png"  },
+    { title: "Islas Baleares", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://residus.caib.es/www/regpig/' target='_blank'>aquí</a>.", modalId: "modal12", iframeSrc: "https://residus.caib.es/www/regpig/", backgroundImage: "baleares.png"  },
+    { title: "Islas Canarias", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://www.gobiernodecanarias.org/medioambiente/materias/residuos/registro-de-produccion/buscador-de-productores/' target='_blank'>aquí</a>.", modalId: "modal13", iframeSrc: " ", backgroundImage: "canarias.png"  },
+    { title: "La Rioja", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://www.larioja.org/medio-ambiente/es/residuos/buscador-nimas' target='_blank'>aquí</a>.", modalId: "modal14", iframeSrc: "https://www.larioja.org/medio-ambiente/es/residuos/buscador-nimas", backgroundImage: "larioja.png"  },
+    { title: "Melilla", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://www.melilla.es/melillaportal/contenedor.jsp?seccion=s_fdes_d4_v1.jsp&contenido=25815&nivel=1400&tipo=6&codResi=1&language=es&codMenu=705&codMenuPN=601&codMenuSN=8' target='_blank'>aquí</a>.", modalId: "modal15", iframeSrc: "https://www.melilla.es/melillaportal/contenedor.jsp?seccion=s_fdes_d4_v1.jsp&contenido=25815&nivel=1400&tipo=6&codResi=1&language=es&codMenu=705&codMenuPN=601&codMenuSN=8", backgroundImage: "melilla.png"  },
+    { title: "Murcia", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://caamext.carm.es/calaweb/faces/faces/vista/seleccionNima.jsp' target='_blank'>aquí</a>.", modalId: "modal16", iframeSrc: "https://caamext.carm.es/calaweb/faces/faces/vista/seleccionNima.jsp", backgroundImage: "murcia.png"  },
+    { title: "Navarra", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://extra.navarra.es/InformacionPublicaPRTR/RPGR.html#/PGR' target='_blank'>aquí</a>.", modalId: "modal17", iframeSrc: "https://extra.navarra.es/InformacionPublicaPRTR/RPGR.html#/PGR", backgroundImage: "navarra.png"  },
+    { title: "País Vasco", description: "En caso de que el botón 'Consultar NIMA' no funcione, presione <a href='https://www.euskadi.eus/informacion/registro-de-produccion-y-gestion-de-residuos/web01-a2inghon/es/' target='_blank'>aquí</a>.", modalId: "modal18", iframeSrc: "https://www.euskadi.eus/informacion/registro-de-produccion-y-gestion-de-residuos/web01-a2inghon/es/", backgroundImage: "paisvasco.png"  },
+];
+
+const cardsContainer = document.getElementById("cards-container");
+const modalsContainer = document.getElementById("modals-container");
+
+let currentRow;
+
+// Generate cards and modals
+cardsData.forEach((card, index) => {
+    if (index % 3 === 0) {
+        currentRow = document.createElement('div');
+        currentRow.className = 'row';
+        cardsContainer.appendChild(currentRow);
+    }
+
+    const uniqueId = `${card.modalId}_${index}`;
+    currentRow.innerHTML += `
+        <div class="col-md-4">
+            <div class="card-flip">
+                <div class="card-flip-inner">
+                    <div class="card-front" style="background-image: url('${card.backgroundImage}');">
+                        <h5 class="card-title">${card.title}</h5>
+                    </div>
+                    <div class="card-back">
+                        <p>${card.description}</p>
+                        <br>
+                        <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#${uniqueId}">CONSULTAR NIMA</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    modalsContainer.innerHTML += `
+        <div class="modal fade" id="${uniqueId}" tabindex="-1" aria-labelledby="${uniqueId}Label" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="${uniqueId}Label">${card.title}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+                    </div>
+                    <div class="modal-body">
+                        <iframe src="${card.iframeSrc}" width="100%" height="500px"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+});
